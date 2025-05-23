373d608d4b5db69f7198d804210860d60aee61e6
abbe <albert.silfverberg@gmail.com>
2025-05-23 16:44:17 +0200

    new exception and class for history feature

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -1,15 +1,23 @@
 import lombok.*;
 import lombok.extern.java.*;
+import java.util.LinkedList;
 
 @Log
 @Getter
-@AllArgsConstructor
 @ToString
 public class Product {
     private String name;
     private double price;
     private int amount;
     private String description;
+    LinkedList<ProductHistory> history = new LinkedList<>();
+
+    public Product(String name, double price, int amount, String description){
+        this.name = name;
+        this.price = price;
+        this.amount = amount;
+        this.description = description;
+    }
 
 
 }
---
+++ src/main/java/ProductHistory.java
@@ -0,0 +1,8 @@
+import lombok.*;
+
+@AllArgsConstructor
+@ToString
+public class ProductHistory {
+    private double price;
+    private int quantity;
+}
