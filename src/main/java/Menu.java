ee57d2a46fa078eaec9ebafa036bd71fbaf54d41
abbe <albert.silfverberg@gmail.com>
2025-05-24 17:10:31 +0200

    corrected

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -14,7 +14,7 @@ public class Menu {
     public void startMenu(){
         while(true) {
             user.userStatus();
-            log.info("What do you want to do?\n1. Search for Product\n2. Log into an authorized account\n3. Add Product\n4. Log off an authorized account\n5. Exit ");
+            log.info("What do you want to do?\n1. Search for Product\n2. Show all available Products\n3. Add Product\n4. Log into an authorized account\n5. Log off an authorized account\n6. Exit ");
 
             try {
                 int temp = Integer.parseInt(userInput.nextLine());
@@ -26,12 +26,7 @@ public class Menu {
                         if (user.searchIsSuccessful()) buyMenu();
                         break;
                     case 2:
-                        log.warning("User chose 2\nLogging in...");
-                        try {
-                            user.userLogIn();
-                        } catch (NotAuthorizedException e) {
-                            log.warning(e.getMessage());
-                        }
+                        log.warning("User chose 2\nShowing all available products...");
                         break;
                     case 3:
                         log.warning("User chose 3\nAdding product...");
@@ -42,15 +37,23 @@ public class Menu {
                         }
                         break;
                     case 4:
-                        log.warning("User chose 4\nLogging off...");
+                        log.warning("User chose 4\nLogging in...");
+                        try {
+                            user.userLogIn();
+                        } catch (NotAuthorizedException e) {
+                            log.warning(e.getMessage());
+                        }
+                        break;
+                    case 5:
+                        log.warning("User chose 5\nLogging off...");
                         try {
                             user.userLogOff();
                         } catch (NotAuthorizedException e) {
                             log.warning(e.getMessage());
                         }
                         break;
-                    case 5:
-                        log.warning("User chose 5\nExiting...");
+                    case 6:
+                        log.warning("User chose 6\nExiting...");
                         countdown();
                         log.warning("Thank you and goodbye!");
                         return;
